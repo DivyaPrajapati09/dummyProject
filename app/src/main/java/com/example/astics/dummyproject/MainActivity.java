2c203d1f6a7266ebfb8d425cd86b72605afb0525
divya4337 <divya4337@gmail.com>
2017-03-20 15:52:21 +0530

    initial commit in dummy project

---
+++ app/src/main/java/com/example/astics/dummyproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.astics.dummyproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
+
+//sudo apt-get install git
